cbde19f2cc634ac89654e2f049ce222c9541c85d
Vlad-code-c <vladz1992b@gmail.com>
2020-04-19 16:41:49 +0300

    Updates

---
+++ precache-manifest.65aeaf88bf6a4728994b98f0d1a31168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769a5455dd2275b33e134939d5729160",
+    "url": "/sql-data-insert/index.html"
+  },
+  {
+    "revision": "68bce0187eb94e6c2e50",
+    "url": "/sql-data-insert/static/css/2.071bea3e.chunk.css"
+  },
+  {
+    "revision": "5b990e347bb7408c9e40",
+    "url": "/sql-data-insert/static/css/main.e330b978.chunk.css"
+  },
+  {
+    "revision": "68bce0187eb94e6c2e50",
+    "url": "/sql-data-insert/static/js/2.3e4aa571.chunk.js"
+  },
+  {
+    "revision": "88cd7f3ac42c5682ad16bdf807cbcd6f",
+    "url": "/sql-data-insert/static/js/2.3e4aa571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b990e347bb7408c9e40",
+    "url": "/sql-data-insert/static/js/main.b77b12c3.chunk.js"
+  },
+  {
+    "revision": "70734de403d36989867a",
+    "url": "/sql-data-insert/static/js/runtime-main.35482112.js"
+  }
+]);
